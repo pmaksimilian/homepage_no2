0d5609e3e54642933f91dcc38de7c42bc1cd8e7d
Maksimilian Pavsic <pavsicmaks@gmail.com>
2019-10-09 14:20:36 +0200

    Adding projects

---
+++ dist/projects/homepage/script.js
@@ -0,0 +1,39 @@
+$(".naslovna-efekt").fadeIn(4000);
+
+$(".naslovna-tekst").fadeIn(2500);
+
+$(".naslovna-tekst").animate({
+  left: '+=70%',
+}, 3000, function() {
+    // Animation complete.
+});
+
+$(".pokaziKdo").click(function() {
+    $(".skritiKdo").slideDown("slow");
+    $(".desni").show("slide", { direction: "right" }, 1500);
+    $(".levi").show("slide", { direction: "left" }, 1500);
+    $(".skrijKdo").show();
+    $(this).hide();
+});
+
+$(".skrijKdo").click(function() {
+    $(".skritiKdo").slideUp("slow");
+    $(".desni").hide();
+    $(".levi").hide();
+    $(".pokaziKdo").show();
+    $(this).hide();
+});
+
+$(".pokaziKaj").click(function() {
+    $(".skritiKaj").slideDown("slow");
+    $(".kvadrat").fadeIn(2000);
+    $(".skrijKaj").show();
+    $(this).hide();
+});
+
+$(".skrijKaj").click(function() {
+    $(".skritiKaj").slideUp("slow");
+    $(".kvadrat").hide();
+    $(".pokaziKaj").show();
+    $(this).hide();
+});
